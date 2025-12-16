739ca5149afcb47ec84873dfa9c9abeb6a7f97ba
NicholasPianfetti <145074360+NicholasPianfetti@users.noreply.github.com>
2025-12-16 01:22:41 -0500

    added config changes for gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import Header from "./Header";
 import Hero from "./Hero";
-import SocialProof from "./SocialProof";
+// import SocialProof from "./SocialProof";
 import About from "./About";
 import HowItWorks from "./HowItWorks";
-import Testimonials from "./Testimonials";
+// import Testimonials from "./Testimonials";
 import FAQ from "./FAQ";
 import Footer from "./Footer";
 
